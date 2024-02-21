8e1ebd99dfb6a363c6485de1d4f98f7e03c00591
vrk0ranit <ranitbag49@gmail.com>
2024-02-21 20:47:16 +0530

    skill

--- src/containers/Skills/Index.js
+++ src/containers/Skills/Index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { BsInfoCircleFill } from 'react-icons/bs'
 import PageHeaderContent from '../../components/pageHeaderContent/Index'
+import { skillsData } from './utils'
+import { Animate, AnimateKeyframes } from 'react-simple-animate'
+import { Line} from 'rc-progress'
+import './styles.scss'
 
 const Skills = () => {
   return (
@@ -9,6 +13,54 @@ const Skills = () => {
         headerText = "My Skills"
         icon={<BsInfoCircleFill size={40}/>}
        />
+       <div className='skills__content-wraper'>
+        {
+          skillsData.map((item,i) =>(
+            <div key={i} className='skills__content-wraper__inner-content'>
+              <Animate
+              play
+              duration={1}
+              delay={0.3}
+              start={{
+                transform: 'translateX(-200px)'
+              }}
+              end={{
+                transform:  'translateX(0px)'
+              }}
+              >
+                <h3 className='skills__content-wraper__inner-content__category-text'>{item.label}</h3>
+                <div className='skills__content-wraper__inner-content__progressbar-container'>
+                  {
+                    item.data.map((skillitem, j) => (
+                      <AnimateKeyframes
+                      play
+                      duration={1}
+                      keyframes={["opacity : 1", "opacity: 0"]}
+                      iterationCount="1"
+                      >
+                        <div className='progressbar-wrapper' key={j}>
+                          <p>{skillitem.skillName}</p>
+                          <Line
+                          percent={skillitem.percentage}
+                          strokeWidth="2"
+                          strokeColor="var( --yellow-theme-main-color)"
+                          trailWidth={"2"}
+                          strokeLinecap="square"
+                          />
+
+                        </div>
+                      </AnimateKeyframes>
+                    ))
+                  }
+                </div>
+
+              </Animate>
+
+            </div>
+          ))
+        }
+
+       </div>
     </section>
   )
 }
---
+++ src/containers/Skills/utils.js
@@ -0,0 +1,59 @@
+export const skillsData = [
+    {
+        label: "FRONT END",
+        data: [
+            {
+                skillName: "HTML",
+                percentage: "90,"
+            },
+            {
+                skillName: "CSS",
+                percentage: "50,"
+            },
+            {
+                skillName: "JAVASCRIPT",
+                percentage: "80,"
+            },
+            {
+                skillName: "REACT JS",
+                percentage: "65"
+            },
+        ],
+    },
+    {
+        label: "BACK END",
+        data: [
+            {
+                skillName: "NODE JS",
+                percentage: "70"
+            },
+            {
+                skillName: "EXPRESS JS",
+                percentage: "50,"
+            },
+            {
+                skillName: "NEXT JS",
+                percentage: "70"
+            },
+        ],
+    },
+    {
+        label: "DATABASE",
+        data: [
+            {
+                skillName: "ORACLE",
+                percentage: "90,"
+            },
+            {
+                skillName: "MONGODB",
+                percentage: "50,"
+            },
+            {
+                skillName: "SQL",
+                percentage: "80,"
+            },
+        ],
+    },
+
+
+]
